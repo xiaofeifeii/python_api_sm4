f1a923b931bca09022113568de65843de70e0311
yangyifei <yangyifei@oceiot.com>
2022-11-24 17:33:07 +0800

    sm4加密完成

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import time
 import sm3, sm4
 from hashlib import md5
-import json
+import requests,json
 import aes
 from Crypto.Cipher import AES
+import getopt
+import sys
 
-# t = int(time.time())
-t = 1640966400
+t = int(time.time())
+# t = 1640966400
 key = 'VILv&y&PB%h^ATND6hcy!zAAHS_DT!T%'
 t_key = hex(~t ^ 0xffffffff).upper()
-# a = int(t_key, 16)
-# a += 1
-# t_key = hex(a).upper()[3:]
-t_key = t_key[3:]
+a = int(t_key, 16)
+a += 1
+t_key = hex(a).upper()[3:]
+# t_key = t_key[3:]
 t_hex = hex(t).upper()[2:]
-
+str1 = key[0:8] + t_key + key[16:24]
+str2 = key[8:16] + t_hex + key[24:32]
+vid1 = sm3.sm3(str1).upper()
+vid2 = sm3.sm3(str2).upper()
+vid_target = vid1 + vid2
+# print(bytes.fromhex(vid_target))
+vid = md5(bytes.fromhex(vid_target)).hexdigest().upper()
 
 def heartbeat():
     # VILv&y&P9E30D7006hcy!zAA
-    str1 = key[0:8] + t_key + key[16:24]
-    str2 = key[8:16] + t_hex + key[24:32]
-    vid1 = sm3.sm3(str1).upper()
-    vid2 = sm3.sm3(str2).upper()
-    vid_target = vid1 + vid2
-    # print(bytes.fromhex(vid_target))
-    vid = md5(bytes.fromhex(vid_target)).hexdigest().upper()
+
     js_heartbeat = {
         "time": t,
         "TID": t_key,
@@ -34,31 +36,47 @@ def heartbeat():
     print(js_str)
 
 
-def encryption_data():
-    data = "Hello World"
+def encryption_data(data):
+    # data = "Hello World"
     t_str = t_key + t_hex
-    str1 = md5(bytes.fromhex(t_str)).hexdigest().upper()
+    md5_str = md5(bytes.fromhex(t_str)).hexdigest().upper()
     aes_key = key[0:16].encode('utf-8')
-    aes_iv = str1[0:16].encode("utf-8")
-
-    aes_data = aes.AEScryptor(aes_key, AES.MODE_CBC, aes_iv, paddingMode="ZeroPadding", characterSet='utf-8')
-    rData = aes_data.encryptFromString(data)
+    aes_iv = md5_str[0:16].encode("utf-8")
+    # print(f"{aes_key}  {aes_iv}")
+    aes_data = aes.AEScryptor(aes_key, AES.MODE_CBC, aes_iv, paddingMode="PKCS7Padding", characterSet='utf-8')
+    rData = aes_data.encryptFromString(str(data))
 
     encryption_aes = rData.toBase64()
+    sm4_key = key[16:32]
+    sm4_iv = md5_str[16:32]
+    # print(f"{sm4_key} and  {sm4_iv}")
+    sm4_data = sm4.encrypt_sm4(sm4_iv, encryption_aes, sm4_key)
+    js_sm4={
+        "time": t,
+        "TID": t_key,
+        "VID": vid,
+        "DATA": sm4_data
+    }
+    print(json.dumps(js_sm4))
 
-    print(encryption_aes)
-
-    # print(key[15:32])
-
-    # sm4_key = md5(bytes.fromhex(key[15:32])).hexdigest().upper()
-    # sm4_iv = bytes.fromhex(str1[15:32])
-    # key2 = bytes.fromhex("0123456789ABCDEFFEDCBA9876543210")  # 128bit密钥
-    # key2 = bytes.fromhex(sm4_key)  # 128bit密钥
-    # plaintext = bytes.fromhex(encryption_aes)  # 128bit明文
-    # plaintext = bytes.fromhex("00112233445566778899aabbccddeeff")  # 128bit明文
-    # SM4 = sm4.SM4Cipher(key2)
-    # print(SM4.encrypt(plaintext).hex())  # 09325c4853832dcb9337a5984f671b9a
+def post_msg(msg):
+    url = 'https://demo.ynyrkj.cn:20016/HostSiteApi.ashx'
+    requests.post(url,data=[("jsonString", json.dumps(msg))])
 
 
+# print(json.dumps(d))
 # heartbeat()
-encryption_data()
+# encryption_data("hello")
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'td:')
+    # print(opts)
+    # print(args)
+    for opt, arg in opts:
+        if opt == '-t':
+            heartbeat()
+        elif opt == '-d':
+            print(arg)
+            encryption_data(arg)
+except getopt.GetoptError as e:
+    print('getopt error:', e)
