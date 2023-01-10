79712d595004c2a36015aa56222b1db4579a4a08
xiaofeifeii <95302248+xiaofeifeii@users.noreply.github.com>
2023-01-10 23:00:18 +0800

    添加mqtt消息发布

--- mqtt_msg.py
+++ mqtt_msg.py
@@ -30,6 +30,20 @@ def subscribe(client: mqtt_client):
     client.subscribe(topic)
     client.on_message = on_message
 
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
 
 def run():
     client = connect_mqtt()
